d6e9e5eb342be62b49b4ac3bbddb59a3bc8632a4
deba1275 <140979881+deba1275@users.noreply.github.com>
2024-01-25 13:45:04 +0100

    Create hello-openshift-for-developers.go

---
+++ hello-openshift-for-developers.go
@@ -0,0 +1,22 @@
+var httpRequestsTotal = prometheus.NewCounter(
+    prometheus.CounterOpts{
+            Name: "http_requests_total",
+            Help: "Total number of http requests.",
+    },
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    httpRequestsTotal.Inc()
+    msg := "Received a request"
+    fmt.Fprint(w, msg)
+    fmt.Println(msg)
+}
+
+func main() {
+    port := "8080"
+    prometheus.MustRegister(httpRequestsTotal)
+    http.HandleFunc("/", handler)
+    http.Handle("/metrics", promhttp.Handler())
+    log.Printf("Server started on port %v", port)
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
